17114b255065b85fd5bf11d18e82995d2b3c0035
levinz97 <levinzeng97@gmail.com>
2021-04-07 12:26:50 +0200

    bugfixed brightness for color image

--- cv20_prog_test/cpp/main.cpp
+++ cv20_prog_test/cpp/main.cpp
@@ -3,15 +3,15 @@
 using namespace std;
 
 #include <opencv2/core/core.hpp>
-
 #include <opencv2/highgui/highgui.hpp>
 
 int main() {
     uchar offset = 150;
     float scaling = 2;
-    string filename;
-    cout << "please input the image name" << endl;
-    cin >> filename;
+    string filename = "baboon.ppm";
+//    cout << "please input the image name" << endl;
+
+//    cin >> filename;
 //    cv::Mat image = cv::imread("head.pgm", cv::IMREAD_GRAYSCALE);
 
     bool colorImage;
@@ -48,20 +48,18 @@ int main() {
     chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
     // visit every pixel with image.at<TYPE>
     for (size_t y = 0; y < image_out.rows; y++){
-        for (size_t x = 0; x < image_out.cols; x++){
-            for(size_t c = 0; c < image_out.channels(); c++){
-                auto intensity =  image_out.at<uchar>(y, x, c);
+        for (size_t x = 0; x < image_out.cols * image_out.channels(); x++){
+//            for(size_t c = 0; c < image_out.channels(); c++){
+                auto intensity =  image_out.at<uchar>(y, x);
 //            cout << intensity << endl;
                 if (intensity + offset > 255){
-                    image_out.at<uchar>(y,x,c)= 255;
+                    image_out.at<uchar>(y,x)= 255;
                 }
                 else{
                     intensity + offset < 0 ?
-                    image_out.at<uchar>(y,x,c) = 0 : image_out.at<uchar>(y,x,c) += offset;
-//                    uchar tmp = image_out.at<uchar>(y,x,c);
-//                    cout << (int)tmp << endl;
+                    image_out.at<uchar>(y,x) = 0 : image_out.at<uchar>(y,x) += offset;
                 }
-            }
+//            }
         }
     }
 
@@ -74,59 +72,59 @@ int main() {
     if(!colorImage) cv::imwrite("offset.pgm", image_out);
     else cv::imwrite("offset.ppm", image_out);
 
-//     visit every pixel with the pointer
-
-    cv::Mat image_out1 = image.clone();
-    for (size_t y = 0; y < image_out1.rows; y++){
-        auto *row_ptr = image.ptr<uchar>(y);
-        for(size_t x = 0; x < image_out1.cols; x++) {
-            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
-            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
-                for(int z = 0; z < 10&& x<1 && y <1; z++)
-                    cout << "before:  "<<(int)*data_ptr << endl;
-//                cout << "after:   "<< (int)*data_ptr << endl;
-            }
-        }
-    }
-    for (size_t y = 0; y < image_out1.rows; y++){
-        auto *row_ptr = image.ptr<uchar>(y);
-        for(size_t x = 0; x < image_out1.cols; x++) {
-            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
-            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
-//                cout << "before:  "<<(int)*data_ptr << endl;
-                *data_ptr -= 127.5;
-                *data_ptr *= scaling;
-                *data_ptr += 127.5;
-                if(*data_ptr < 0) *data_ptr = 0;
-                if(*data_ptr > 255) *data_ptr = 255;
-//                cout << "after:   "<< (int)*data_ptr << endl;
-            }
-        }
-    }
-    ///////////////////////////////////
-    for (size_t y = 0; y < image_out1.rows; y++){
-        auto *row_ptr = image.ptr<uchar>(y);
-        for(size_t x = 0; x < image_out1.cols; x++) {
-            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
-            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
-                for(int z = 0; z < 10 && x<1 && y <1; z++)
-                    cout << "now:  "<<(int)*data_ptr << endl;
-//                cout << "after:   "<< (int)*data_ptr << endl;
-            }
-        }
-    }
-
-    cv::imshow("scaling", image_out1);
-    if(!colorImage) cv::imwrite("scaling.pgm", image_out1);
-    else cv::imwrite("scaling.ppm", image_out1);
-    cv::waitKey();
-    cv::destroyAllWindows();
-
-    // automatically called at the end of a scope
-    // not needed unless the matrix size varies in different iterations within same loop
-    image.release();
-    image_out.release();
-    image_out1.release();
+////     visit every pixel with the pointer
+//
+//    cv::Mat image_out1 = image.clone();
+//    for (size_t y = 0; y < image_out1.rows; y++){
+//        auto *row_ptr = image.ptr<uchar>(y);
+//        for(size_t x = 0; x < image_out1.cols; x++) {
+//            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
+//            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
+//                for(int z = 0; z < 10&& x<1 && y <1; z++)
+//                    cout << "before:  "<<(int)*data_ptr << endl;
+////                cout << "after:   "<< (int)*data_ptr << endl;
+//            }
+//        }
+//    }
+//    for (size_t y = 0; y < image_out1.rows; y++){
+//        auto *row_ptr = image.ptr<uchar>(y);
+//        for(size_t x = 0; x < image_out1.cols; x++) {
+//            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
+//            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
+////                cout << "before:  "<<(int)*data_ptr << endl;
+//                *data_ptr -= 127.5;
+//                *data_ptr *= scaling;
+//                *data_ptr += 127.5;
+//                if(*data_ptr < 0) *data_ptr = 0;
+//                if(*data_ptr > 255) *data_ptr = 255;
+////                cout << "after:   "<< (int)*data_ptr << endl;
+//            }
+//        }
+//    }
+//    ///////////////////////////////////
+//    for (size_t y = 0; y < image_out1.rows; y++){
+//        auto *row_ptr = image.ptr<uchar>(y);
+//        for(size_t x = 0; x < image_out1.cols; x++) {
+//            uchar *data_ptr = &row_ptr[x * image_out1.channels()];
+//            for(int c = 0; c != image_out1.channels(); c++,data_ptr++){
+//                for(int z = 0; z < 10 && x<1 && y <1; z++)
+//                    cout << "now:  "<<(int)*data_ptr << endl;
+////                cout << "after:   "<< (int)*data_ptr << endl;
+//            }
+//        }
+//    }
+//
+//    cv::imshow("scaling", image_out1);
+//    if(!colorImage) cv::imwrite("scaling.pgm", image_out1);
+//    else cv::imwrite("scaling.ppm", image_out1);
+//    cv::waitKey();
+//    cv::destroyAllWindows();
+//
+//    // automatically called at the end of a scope
+//    // not needed unless the matrix size varies in different iterations within same loop
+//    image.release();
+//    image_out.release();
+//    image_out1.release();
     return 0;
 
 }
